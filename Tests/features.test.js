a03cad7836a17c97f4d4ae7c59f5e5600cf4510f
KT <24013921@myrp.edu.sg>
2026-01-21 18:20:36 +0800

    rearranged the codes for cleearer view

--- Tests/features.test.js
+++ Tests/features.test.js
@@ -52,13 +52,13 @@ expect(res.statusCode).toBe(200);
 
 //Marking tasks complete
 app.post('/complete/:id', (req, res) => {
-  const task = tasks.find(t => t.id == req.params.id);
+const task = tasks.find(t => t.id == req.params.id);
 
-  if (task) {
-    task.completed = !task.completed;
-  }
+if (task) {
+task.completed = !task.completed;
+}
 
-  res.redirect('/');
+res.redirect('/');
 });
 
 //Invalid input
